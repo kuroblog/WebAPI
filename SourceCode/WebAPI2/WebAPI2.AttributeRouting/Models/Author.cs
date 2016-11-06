b0140f6b70cf86c1f04990246d1170711362dfe8
Kuro <kuro_q@live.com>
2016-11-06 17:23:16 +0800

    add class Author

---
+++ SourceCode/WebAPI2/WebAPI2.AttributeRouting/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WebAPI2.AttributeRouting.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
