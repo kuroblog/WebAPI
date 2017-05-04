31e7fed29b605ee767e3fc6bd3b2c7f50ee35c33
kuro <kuro_q@live.com>
2017-05-05 01:58:39 +0800

    -> add convert and verify

---
+++ SourceCode/WebAPI2/DataConvert.WebApi/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿
+namespace DataConvert.WebApi.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Hello([FromUri]string user = "guest")
+        {
+            var resultMessage = await Task.FromResult($"Hello {user}, Welcome to access my api on {DateTime.UtcNow}");
+
+            return Ok(resultMessage);
+        }
+    }
+}
