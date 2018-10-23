96bcebf73ed15f652489c45426c961c93d52d347
Yuqi.Zhao <temp777@yeah.net>
2018-10-23 17:12:46 +0800

    impt webservice

---
+++ SourceCode/WebAPI.DotNetCore/WebApp.CallWebService/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿
+namespace WebApp.CallWebService.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using WebApp.CallWebService.Core;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly IHisSvcProxyInterface svcProxy;
+
+        public DefaultController(ServiceFactory svcFactory)
+        {
+            svcProxy = svcFactory.Instance;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var result = await svcProxy.DoTrans(new DoTransArgs("4004", ""));
+
+            return await Task.FromResult(result.JsonResult);
+        }
+    }
+}
---
+++ SourceCode/WebAPI.DotNetCore/WebApp.CallWebService/Startup.cs
@@ -0,0 +1,46 @@
+﻿
+namespace WebApp.CallWebService
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using WebApp.CallWebService.Core;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<HisOptions>(Configuration.GetSection(nameof(HisOptions)));
+
+            services.AddTransient<IHisSvcProxyInterface, HisSocketProxy>();
+            services.AddTransient<IHisSvcProxyInterface, HisWebSvcProxy>();
+            services.AddSingleton<ServiceFactory>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //services.AddScoped<IFoo, Foo>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
